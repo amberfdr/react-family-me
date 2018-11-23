296c332bcbb616e850ed1eb573f6a5aff7491427
amberfdr <amberfdr@163.com>
2018-11-23 18:35:13 +0800

    next redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-/*使用es6箭头函数*/
-var func = src => {
-    document.getElementById('app').innerHTML = src;
-};
-func('我现在使用Babel!');
+import React from 'react';
+import ReactDom from 'react-dom';
+import Hello from 'component/Hello/Hello';
+import { AppContainer } from 'react-hot-loader';
+import getRouter from 'router/router';
+/*初始化*/
+renderWithHotReload(getRouter())
+/*热更新  当模块更新的时候通知index.js*/
+if(module.hot){
+    module.hot.accept('router/router',() => {
+        const getRouter = require('router/router').default;
+        console.log(getRouter());
+        renderWithHotReload(getRouter());
+    });
+}
+
+function renderWithHotReload(RootElement){
+    ReactDom.render(
+        <AppContainer>
+            {RootElement}
+        </AppContainer>,
+        document.getElementById('app'));
+}
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
-console.log(__dirname);
 module.exports = {
-    entry: path.join(__dirname,'src/index.js'),//__dirname变量:程序运行的根目录  join()将字符串拼接在一起
+    entry: [
+        'react-hot-loader/patch',
+        path.join(__dirname,'src/index.js')
+    ],//__dirname变量:程序运行的根目录  join()将字符串拼接在一起
     output:{
         path:path.join(__dirname,'./dist'),
         filename:'bundle.js'
@@ -10,13 +12,28 @@ module.exports = {
         rules:[{
             test:/\.js$/,
             use:[{
-                loader:'babel-loader?catchDirectory=true',
-                query: {
-                    presets: ['@babel/react']
-                  }
+                loader:'babel-loader',
+                // query: {
+                //     presets: ['@babel/react'],
+                //     plugins:[]
+                //   }
             }
             ],
             include:path.join(__dirname,'src')
         }]
+    },
+    devServer:{
+        contentBase:path.join(__dirname,'dist'),
+        port: 9000,
+        historyApiFallback:true,
+        host:'0.0.0.0'
+    },
+    mode:'none',
+    resolve:{
+        alias:{
+            pages:path.join(__dirname,'src/pages'),
+            component:path.join(__dirname,'src/component'),
+            router:path.join(__dirname,'src/router')
+        }
     }
 }
